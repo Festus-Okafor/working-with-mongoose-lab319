1832f16799bbe5d6cb1a82cfdd26d4d32a5b752f
mexie07 <okaforfestus07@gmail.com>
2025-06-22 18:58:41 -0400

    backend connected

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import 'dotenv/config'
 import mongoose from 'mongoose'
+import Blog from './models/blog.js'
 
 
 
 
 
 const app = express()
+app.use(express.json())
 const PORT = process.env.PORT
 await mongoose.connect(process.env.MONGO_URL)
 
@@ -14,7 +16,17 @@ app.get("/", (req, res)=>{
    // let membership = await()
     res.send(`working`)
 })
-
+app.get('/add.blog', async(req, res) =>{
+    const blog = new Blog({
+     blogname: 'MexSktech Tech News Updates',
+     title: 'New updates about Nano-engineering',
+     post_snippet:'Have you heard?',
+     comments: 'I am excited this information is comming to us'
+    })
+     const result = await  blog.save()
+     res.json(result)
+})
+ 
 
 app.listen(PORT, () =>{
     console.log(`Now listening on port: `, PORT)
---
+++ models/blog.js
@@ -0,0 +1,24 @@
+
+
+import mongoose from 'mongoose'
+const schema = mongoose.Schema;
+const blogSchema = new schema({
+
+    blogname: {
+        type: String,
+       
+    },
+    title: {
+        type: String,
+    },
+    post_snippet: {
+        type: String,
+    },
+    comments:{
+     Type: String,
+    }  
+ },
+  {timestamps: true})
+ const Blog = mongoose.model('Blog', blogSchema)
+
+ export default Blog
